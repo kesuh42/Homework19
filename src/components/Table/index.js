77ba2d17a7bb56fdda852c0cca2aaf4623349c59
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-03-12 17:26:28 -0500

    initial test page setup

---
+++ src/components/Table/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Table extends React.Component {
+    state = {
+        filter: "name"
+    }
+
+    render() {
+        return (
+            <div>Test
+            </div>
+        )
+    }
+}
+
+export default Table;
