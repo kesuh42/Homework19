fee481bdedab973260910a8efdce26219aab3c0b
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-03-13 01:10:41 -0500

    basic table render

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,14 +1,56 @@
 import React from "react";
+import API from "../../utils/API"
 
 class Table extends React.Component {
     state = {
-        filter: "name"
+        filter: false,
+        sort: false,
+        userdata: []
     }
 
+    componentDidMount() {
+        this.retrieveUsers()
+      }
+
+    retrieveUsers = () => {
+        API.search()
+        .then(response => this.setState({filter: false, sort: false, userdata: response.data.results}))
+        .then(() => console.log(this.state))
+    }
+
+    iterator = (array) => {
+        let result = []
+        for (let i of array) {
+            result.push(
+            <tr>
+                <td>{i.name.first}</td>
+                <td>{i.name.last}</td>
+                <td>{i.gender}</td>
+                <td>{i.email}</td>
+                <td>{i.phone}</td>
+            </tr>)
+
+        }
+        return result
+    }
+    
     render() {
         return (
-            <div>Test
-            </div>
+            <table>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Gender</th>
+                    <th>Email</th>
+                    <th>Phone Number</th>
+                </tr>
+                {this.iterator(this.state.userdata)}
+            </table>
+
+
+
+            // <div> {this.iterator(this.state.userdata)}
+            // </div>
         )
     }
 }
