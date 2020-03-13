989c4eb09e46b35c2a2f0edc394e60138a01e9b5
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-03-13 15:18:31 -0500

    filter and sort functions

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,8 +3,8 @@ import API from "../../utils/API"
 
 class Table extends React.Component {
     state = {
-        filter: false,
-        sort: false,
+        filter: true,
+        sort: true,
         userdata: []
     }
 
@@ -14,12 +14,30 @@ class Table extends React.Component {
 
     retrieveUsers = () => {
         API.search()
-        .then(response => this.setState({filter: false, sort: false, userdata: response.data.results}))
+        .then(response => this.setState({...this.state, userdata: response.data.results}))
         .then(() => console.log(this.state))
     }
 
     iterator = (array) => {
         let result = []
+
+        //if state.filter is true, run the filter function which includes only women
+        if (this.state.filter) {
+            array = array.filter(person => person.gender === "female")
+        }
+
+        //if state.sort is true, run the sort function which sorts by last name
+        if (this.state.sort) {
+            function compare(a, b) {
+                if (a.name.last > b.name.last) return 1;
+                if (b.name.last > a.name.last) return -1;
+              
+                return 0;
+            }
+
+            array = array.sort(compare)
+        }
+
         for (let i of array) {
             result.push(
             <tr>
